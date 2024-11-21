9dcf8b785fcbbd5704475ab6d40612f07f2e6e2f
Matthew <matthewsaxton@u.boisestate.edu>
2024-11-21 14:00:46 -0700

    added comment editing (still bugged)

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
-db.run(`CREATE TABLE comments (
+db.run(`CREATE TABLE IF NOT EXISTS comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     comment TEXT NOT NULL)`)
 
@@ -23,6 +23,11 @@ app.get('/', function (req, res) {
 })
 
 app.get('/student1', function (req, res) {
+  console.log('GET called')
+  res.render('student1', local)
+})
+
+app.get('/student1/comments', function (req, res) {
   const local = { comments: [] }
   db.each('SELECT id, comment FROM comments', function (err, row) {
     if (err) {
@@ -32,7 +37,7 @@ app.get('/student1', function (req, res) {
     }
   }, function (err, numrows) {
     if (!err) {
-      res.render('student1', local)
+      res.render('student1/comments', local)
     } else {
       console.log(err)
     }
@@ -48,12 +53,30 @@ app.get('/student2', function (req, res) {
 student3Routes(app, db)
 
 
-app.post('/add', function(req, res) {
+app.post('/addstudent1', function(req, res) {
   console.log('adding comment')
   const stmt = db.prepare('INSERT INTO comments (comment) VALUES (?)')
-  stmt.run(rew.body.comment)
+  stmt.run(req.body.comment)
   stmt.finalize()
-  res.redirect('/')
+  res.redirect('/student1/comments')
+})
+
+app.post('/deletestudent1', function (req, res) {
+  console.log('deleting comment')
+  const stmt = db.prepare('DELETE FROM comments where id = (?)')
+  stmt.run(req.body.id)
+  stmt.finalize()
+  res.redirect('/student1/comments')
+
+})
+
+app.post('/editstudent1', function(req, res) {
+  console.log('editing comment')
+  const stmt = db.prepare('UPDATE comments SET comment = (?) WHERE id = (?)')
+  stmt.run(req.body.comment)
+  stmt.run(req.body.id)
+  stmt.finalize()
+  res.redirect('/student1/comments')
 })
 
 // Start the web server

9b9a020ecdcf8e52f8a8ff076a58c8b922081213
Matthew <matthewsaxton@u.boisestate.edu>
2024-11-22 15:06:33 -0700

    Edit Comment, Random Comments

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
-db.run(`CREATE TABLE IF NOT EXISTS comments (
+db.run(`CREATE TABLE IF NOT EXISTS s1comments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     comment TEXT NOT NULL)`)
 
@@ -22,18 +22,33 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
+function getRandomItems(array, numItems) {
+  const shuffled = array.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, numItems);
+}
+
 app.get('/student1', function (req, res) {
-  console.log('GET called')
-  res.render('student1', local)
-})
+  const s1comments = [];
+
+  db.all('SELECT id, comment FROM s1comments', function (err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Database error');
+    } else {
+      const randomComments = getRandomItems(rows, 5); // Fetch 5 random comments
+      res.render('student1', { s1comments: randomComments });
+    }
+  });
+  console.log("GET Called")
+});
 
 app.get('/student1/comments', function (req, res) {
-  const local = { comments: [] }
-  db.each('SELECT id, comment FROM comments', function (err, row) {
+  const local = { s1comments: [] }
+  db.each('SELECT id, comment FROM s1comments', function (err, row) {
     if (err) {
       console.log(err)
     } else {
-      local.comments.push({ id: row.id, comment: row.comment })
+      local.s1comments.push({ id: row.id, comment: row.comment })
     }
   }, function (err, numrows) {
     if (!err) {
@@ -53,31 +68,51 @@ app.get('/student2', function (req, res) {
 student3Routes(app, db)
 
 
-app.post('/addstudent1', function(req, res) {
+app.post('/student1/comments/add', function(req, res) {
   console.log('adding comment')
-  const stmt = db.prepare('INSERT INTO comments (comment) VALUES (?)')
+  const stmt = db.prepare('INSERT INTO s1comments (comment) VALUES (?)')
   stmt.run(req.body.comment)
   stmt.finalize()
   res.redirect('/student1/comments')
 })
 
-app.post('/deletestudent1', function (req, res) {
+app.post('/student1/comments/delete', function (req, res) {
   console.log('deleting comment')
-  const stmt = db.prepare('DELETE FROM comments where id = (?)')
+  const stmt = db.prepare('DELETE FROM s1comments where id = (?)')
   stmt.run(req.body.id)
   stmt.finalize()
   res.redirect('/student1/comments')
 
 })
 
-app.post('/editstudent1', function(req, res) {
-  console.log('editing comment')
-  const stmt = db.prepare('UPDATE comments SET comment = (?) WHERE id = (?)')
-  stmt.run(req.body.comment)
-  stmt.run(req.body.id)
-  stmt.finalize()
-  res.redirect('/student1/comments')
-})
+app.get('/student1/comments/edit/:id', function (req, res) {
+  let id = req.params.id;
+  db.get('SELECT * FROM s1comments WHERE id = ?', [id], (err, comment) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render('student1/edit', { comment: comment });
+  });
+});
+
+
+app.post('/student1/comments/edit/:id', function (req, res) {
+  let id = req.params.id;
+  let updatedComment = req.body.comment;
+
+  console.log(`Updating comment with ID: ${id}, New Comment: ${updatedComment}`);
+
+  db.run('UPDATE s1comments SET comment = ? WHERE id = ?', [updatedComment, id], function (err) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send('Database error');
+    } else {
+      console.log(`Updated comment for student1 with ID: ${id}`);
+      res.redirect('/student1/comments');
+    }
+  });
+});
+
 
 // Start the web server
 app.listen(3000, function () {
